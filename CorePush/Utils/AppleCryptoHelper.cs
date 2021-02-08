e2a35d99a649c9d1335ae5e6a270ca103dfce657
Daniel Cohen Gindi <danielgindi@gmail.com>
2021-02-08 09:54:44 +0200

    Support .NET Standard 2.0 through multitarget build

--- CorePush/Utils/AppleCryptoHelper.cs
+++ CorePush/Utils/AppleCryptoHelper.cs
@@ -1,13 +1,35 @@
 ﻿
 using System;
 using System.Security.Cryptography;
+
+#if NETSTANDARD2_0
 using Org.BouncyCastle.Crypto.Parameters;	
 using Org.BouncyCastle.Security;
+#endif
 
 namespace CorePush.Utils
 {
     public static class AppleCryptoHelper
     {
+#if NETSTANDARD2_1
+        public static ECDsa GetEllipticCurveAlgorithm(string privateKey)
+        {
+            var dsa = ECDsa.Create();
+
+            try
+            {
+                dsa.ImportPkcs8PrivateKey(Convert.FromBase64String(privateKey), out _);
+                return dsa;
+            }
+            catch
+            {
+                dsa.Dispose();
+                throw;
+            }
+        }
+#endif
+
+#if NETSTANDARD2_0
         public static ECDsa GetEllipticCurveAlgorithm(string privateKey)	
         {	
             var keyParams = (ECPrivateKeyParameters) PrivateKeyFactory.CreateKey(Convert.FromBase64String(privateKey));	
@@ -24,5 +46,6 @@ namespace CorePush.Utils
                 }	
             });	
         }
+#endif
     }
 }
